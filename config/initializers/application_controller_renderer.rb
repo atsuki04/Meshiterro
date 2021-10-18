58c76e0269d1f558157691ec2367f5c69609a8d4
atsuki04 <cavu04hmp@gmail.com>
2021-10-18 07:09:01 +0000

    [Finish] 4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd1b88d762cfdd23a24af8d432deb38ecb507cac6b3d942280afb1d0a459662647841a9ce377a25d011576336f79c378b8a0bc21f8bdc306086c1ce3c045848fb'
